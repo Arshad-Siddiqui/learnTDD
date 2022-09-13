97f1d8d5cd1ddec410487850e67bf3b87b0ccf7a
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-13 16:30:52 +0100

    Add make_snippets test and make_snippet file

---
+++ spec/make_snippet_spec.rb
@@ -0,0 +1,20 @@
+require 'make_snippet'
+
+RSpec.describe 'make_snippet method' do
+  it 'returns the same string if 5 words long' do
+    result = make_snippet('a sentence shorter than 5')
+    expect(result).to eq 'a sentence shorter than 5'
+  end
+
+  it 'returns the same string if less than 5 words long' do
+    result = make_snippet('not longer than 5')
+    expect(result).to eq 'not longer than 5'
+  end
+
+  context 'more than 5 words' do
+    it 'truncates the string to 5 words long and adds "..." at the end' do
+      result = make_snippet('a sentence absolutely longer than 5 words long, in fact its 12')
+      expect(result).to eq 'a sentence absolutely longer than...'
+    end
+  end
+end
