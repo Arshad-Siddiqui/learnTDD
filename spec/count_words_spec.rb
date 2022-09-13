7bd62c61b70b63991ceb97adf9ed89e10a69e885
Arshad Siddiqui <ferozarshad123@gmail.com>
2022-09-13 17:13:16 +0100

    Add tests for count_words method

---
+++ spec/count_words_spec.rb
@@ -0,0 +1,19 @@
+require 'count_words'
+
+RSpec.describe 'count_words method' do
+  it 'Returns 0 when an empty string is given' do
+    result = count_words('')
+    expect(result).to eq 0
+  end
+
+  it 'Returns the length when string of length is given' do
+    result = count_words('hello world')
+    expect(result).to eq 11
+  end
+
+  context 'number is given' do
+    it 'throws an error' do
+      expect { count_words(12) } raise_error 'Number provided instead of string'
+    end
+  end
+end
